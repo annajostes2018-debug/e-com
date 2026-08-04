54b5f39ef681f5706da9f75041441693ba5baab8
annajostes2018-debug <annajostes2018@gmail.com>
2026-08-03 21:09:02 -0500

    update

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove("menu--open");
 }
+
+function toggleMenu() {
+    const menu = document.getElementById('menu');
+
+    // Toggle the class that controls visibility
+    if (menu.classList.contains('open')) {
+        menu.classList.remove('open'); // Close the menu
+    } else {
+        menu.classList.add('open'); // Open the menu
+    }
+}
